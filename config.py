2538c34a1062df1eec18b0b9e874e8539392001b
sunjinwoo17 <groottelegram@gmail.com>
2025-01-22 20:08:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ganeshganesh177:17032009@
 DB_NAME = os.environ.get("DATABASE_NAME", "Sasukeuchiha")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002338550202"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
